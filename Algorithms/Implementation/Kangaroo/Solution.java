c6cf7a05376c8f1e35d2785132c88050dd63e390
Tapio Vaattanen <vaattanen@gmail.com>
2016-08-05 17:22:22 -0400

    Update Solution.java

--- Algorithms/Implementation/Kangaroo/Solution.java
+++ Algorithms/Implementation/Kangaroo/Solution.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Solution {
 	/* There certainly is more elegant solution than mine.
-	You could check (x2-x1)%(v1-v2) /*
+	You could check (x2-x1)%(v1-v2) */
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
