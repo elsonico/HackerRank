24013ce5ce34d1d449447618ea050d7061834f79
Sonico Elephas <vaattanen@gmail.com>
2016-08-05 17:03:49 -0400

    Added Kangaroo

---
+++ Algorithms/Implementation/Kangaroo/Solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int x1 = in.nextInt();
+		int v1 = in.nextInt();
+		int x2 = in.nextInt();
+		int v2 = in.nextInt();
+		if ((x1 > x2 && v1 > v2) || (x2 > x1 && v2 > v1) ) {
+			System.out.println("NO");
+			System.exit(0);
+		}
+		if (x1 < x2) {
+			while ( x1 < x2) {
+				x1 = x1+v1;
+				x2 = x2+v2;			
+			}
+		}
+		else {
+			while ( x2 < x1) {
+				x1 = x1+v1;
+				x2 = x2+v2;				
+			}
+		}
+		if (x1==x2) {
+			System.out.println("YES");
+		}
+		else {
+			System.out.println("NO");
+		}
+		in.close();
+	}
+}
